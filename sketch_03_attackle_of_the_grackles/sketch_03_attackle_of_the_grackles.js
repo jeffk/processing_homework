04925ff88b66e5ed1c23c45e677d6a4ec862f0b7
Jeff Kramer <jeff.kramer@voxmedia.com>
2016-11-06 14:13:23 -0600

    Fix for mice on touch devices.

--- sketch_03_attackle_of_the_grackles/sketch_03_attackle_of_the_grackles.js
+++ sketch_03_attackle_of_the_grackles/sketch_03_attackle_of_the_grackles.js
@@ -151,6 +151,7 @@ function draw() {
     truck.move(frame);
     for (var i = 0; i < grackles.length; i++) {
       grackles[i].move(frame, truck);
+      mpressed = false;
     }
   } else {
     crazy_grackles = false;
